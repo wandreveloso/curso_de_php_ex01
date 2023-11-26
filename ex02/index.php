83f73a616aab9816cd859ac9023507eaed681901
wandreveloso <wandreveloso@gmail.com>
2023-11-26 17:44:39 -0300

    Respondendo ao exercício 2

---
+++ ex02/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo "Esta parte será impressa";
+    // Já aqui não será impresso
+    // Feito por Wandré Veloso
+    // Este código está no meu GitHub: https://github.com/wandreveloso/
+    // Sou professor de computação
+
+?>
