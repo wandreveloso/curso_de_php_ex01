32c73637362d25d25d2304db2c2ab34534241259
wandreveloso <wandreveloso@gmail.com>
2023-11-26 18:16:06 -0300

    Respondendo ao exercício 3

---
+++ ex04/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo 15.5; //primeiro float
+    echo "<br>";
+    $numero = 1584.65+81; //segundo float
+    echo $numero;
+    echo "<br>";
+    if($numero == true){
+        echo "O segundo número é float";
+        echo "<br>";
+    }
+    $numero_int = 5; //esse é int, mas será float também?
+    echo $numero_int;
+    echo "<br>";
+    if($numero_int == true){
+        echo "O terceiro número é int e float";
+        echo "<br>";
+    }
+
+?>
