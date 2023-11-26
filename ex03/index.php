3358ebcbb9bf1b89de741574143aca3ba2e27932
wandreveloso <wandreveloso@gmail.com>
2023-11-26 17:59:16 -0300

    Respondendo ao exercício 3

---
+++ ex03/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo 3; //primeiro número inteiro
+    echo "<br>";
+    echo 5+7; //segundo número inteiro
+    echo "<br>";
+    $numero = 35*984; //variável a ser impressa
+    echo $numero;
+
+?>
