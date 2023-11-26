eb9fc5a06f997436d90da75d34c69548f7be7c42
wandreveloso <wandreveloso@gmail.com>
2023-11-26 16:54:58 -0300

    exercício resolvido

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Link do curso: https://www.udemy.com/course/php-do-zero-a-maestria-com-projetos-incriveis/
+    // Ex01: Crie um arquivo PHP contendo uma instrução que imprima seu nome e execute o arquivo
+    //Este é um comentário e não vai rodar no código
+    //O echo imprime o conteúdo na tela
+    //Após o echo eu posso colocar códigos HTML
+    //Basta colocar este arquivo na pasta /var/www/html e abrir no navegador em localhost/index.php
+    echo "Wandré Veloso"
+
+?>
