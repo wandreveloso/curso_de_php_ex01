97ed17249da47fa06a3c7ef793c1b380f25fc9f3
wandreveloso <wandreveloso@gmail.com>
2023-11-27 10:54:14 -0300

    Resolvendo o exercício 5

---
+++ ex05/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = "Wandré"; //aqui estou declarando uma variável string
+    if(is_string($nome)){ //verifico se a variável é uma string
+        echo "Sim, '$nome' é uma string<br>";
+    }
+    echo 'Agora, usando apóstrofes, a variável $nome é impressa como ela é chamada, literalmente, não o seu valor<br>';
+
+?>
