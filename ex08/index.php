4f19dbbb019a4d195cf154030404a275f5417e37
wandreveloso <wandreveloso@gmail.com>
2023-11-27 12:52:06 -0300

    Resolvendo o exercício 8

---
+++ ex08/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $var1 = 75; //tipo int
+    $var2 = 98.53; //tipo float
+    $var3 = true; //tipo bool
+    $var4 = "Wandré"; //tipo string
+    $var5 = NULL; //tipo NULL
+    echo "Aqui a var1: '$var1'";
+    echo "<br>";
+    echo "Aqui a var2: '$var2'";
+    echo "<br>";
+    echo "Aqui a var3: '$var3' (embora seja true, imprime 1)";
+    echo "<br>";
+    echo "Aqui a var4: '$var4'";
+    echo "<br>";
+    echo "Aqui a var5: '$var5' (não imprime nada porque é NULL)";
+    echo "<br>";
+
+?>
